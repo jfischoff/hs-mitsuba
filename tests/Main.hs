ca067e731bd0dadb994d127cfd108cacd28e9c5e
Jonathan Fischoff <jonathangfischoff@gmail.com>
2014-03-11 22:39:04 -0700

    fixed cabal file

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 import qualified Tests.Mitsuba.Types as Types
-import qualified Tests.Mitsuba.Class as Class
+--import qualified Tests.Mitsuba.Class as Class
 import Test.Tasty
 
 
 main = defaultMain $ testGroup 
    "main" 
       [ Types.tests
-      , Class.tests
+--      , Class.tests
       ]
